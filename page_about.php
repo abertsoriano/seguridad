fd30625a38a7e2bfc9f96403d5e6c52eeffe4dd8
Oscar Larriega - Personal Macbook Pro <larriega@gmail.com>
2015-11-27 01:29:44 -0500

    pagina nosotros adaptado al nuevo diseño

---
+++ page_about.php
@@ -0,0 +1,126 @@
+<?php include 'tpl/header.tpl.html' ?>
+    <!--=== Breadcrumbs ===-->
+    <div class="breadcrumbs breadcrumbs-dark">
+        <div class="container">
+            <h1 class="pull-left">About Me</h1>
+            <ul class="pull-right breadcrumb">
+                <li><a href="index.php">Home</a></li>
+                <li class="active">About me</li>
+            </ul>
+        </div>
+    </div>
+    <!--=== End Breadcrumbs ===-->
+
+    <!--=== Content Part ===-->
+    <div class="container content -sm">
+        <div class="row about-me">
+            <div class="col-sm-4 shadow-wrapper md-margin-bottom-40">
+                <div class="box-shadow shadow-effect-2">
+                    <img class="img-responsive img-bordered full-width" src="assets/img/team/imgnosotros.jpg" alt="">
+                </div>
+            </div>
+
+            <div class="col-sm-8">
+                <div class="overflow-h">
+
+                    <ul class="social-icons pull-right">
+                        <li><a class="rounded-x social_facebook" data-original-title="Facebook" href="#"></a></li>
+                        <li><a class="rounded-x social_twitter" data-original-title="Twitter" href="#"></a></li>
+                        <li><a class="rounded-x social_googleplus" data-original-title="Google Plus" href="#"></a></li>
+                        <li><a class="rounded-x social_linkedin" data-original-title="Linkedin" href="#"></a></li>
+                    </ul>
+                </div>
+                <p>CONTROL EMPRESARIAL GROUP SAC es una organización empresarial enmarcada en lineamientos estratégicos y operativos internacionales, los cuales están siendo aplicados en nuestro medio por nuestros Directores Mario Mallca y Miguel Sánchez, amplios conocedores en materias de seguridad, controles para empresas, medidas de protección para ejecutivos, prevención y control de pérdidas.</p>
+                <p>Nuestra empresa está conformada por personal calificado con una amplia experiencia en los temas de protección y control de pérdidas en las empresas. Contamos con las mejores alternativas para su empresa en el tema de protección y prevención; básicamente, por un conjunto de medidas analizadas y puestas en práctica en instituciones que ya cuentan con estos servicios con resultados positivos.</p>
+                <p>Contamos con nuestra Area de Capacitación e implementamos equipos especiales de protección de ejecutivos y alta gerencia, los cuales son adiestrados en las tareas de:</p>
+                <ul class="list-unstyled">
+                    <li><i class="fa fa-check color-green"></i> Inteligencia</li>
+                    <li><i class="fa fa-check color-green"></i> Planificación operativa</li>
+                    <li><i class="fa fa-check color-green"></i> Contra espionaje industrial (fuga de información</li>
+                    <li><i class="fa fa-check color-green"></i> Técnicas de rescate</li>
+                    <li><i class="fa fa-check color-green"></i> Comunicaciones y uso de equipos electrónicos</li>
+                </ul>
+                <br/>
+                <blockquote class="hero-unify">
+                    <p>Nuestro potencial profesional humano se encuentra respaldado por la utilización de los avances tecnológicos más modernos en Control Interno, Auditoria, Selección e Investigación de Personal, Protección Física, Canina, Seguridad Electrónica, Investigaciones y Proyectos.</p>
+                    <small>CEO Jack Bour</small>
+                </blockquote>
+            </div>
+        </div><!--/row-->
+
+    	<!-- Meer Our Team -->
+    	<div class="headline"><h2>Meet Our Team</h2></div>
+        <div class="row team">
+            <div class="col-sm-3">
+                <div class="thumbnail-style">
+                    <img class="img-responsive" src="assets/img/team/nosotros1.jpg" alt="" />
+                    <h3><a>Mario Mallca</a> <small>Gerente General</small></h3>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, justo sit amet risus etiam porta sem...</p>
+                    <ul class="list-unstyled list-inline team-socail">
+                    	<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    	<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    	<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="thumbnail-style">
+                    <img class="img-responsive" src="assets/img/team/nosotros1.jpg" alt="" />
+                    <h3><a>Miguel Sanchez</a> <small>Gerente de Operaciones</small></h3>
+                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, justo sit amet risus etiam porta sem...</p>
+                    <ul class="list-unstyled list-inline team-socail">
+                    	<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    	<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    	<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div><!--/team-->
+    	<!-- End Meer Our Team -->
+
+        <!-- Owl Clients v1 -->
+        <div class="headline"><h2>Our Clients</h2></div>
+        <div class="owl-clients-v1">
+            <div class="item">
+                <img src="assets/img/clients4/11.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="assets/img/clients4/12.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="assets/img/clients4/13.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="assets/img/clients4/14.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="assets/img/clients4/15.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="assets/img/clients4/16.jpg" alt="">
+            </div>
+            <div class="item">
+                <img src="assets/img/clients4/17.jpg" alt="">
+            </div>
+        </div>
+        <!-- End Owl Clients v1 -->
+    </div><!--/container-->
+<?php include 'tpl/footer.tpl.php' ?>
+<!-- JS Implementing Plugins -->
+<script type="text/javascript" src="assets/plugins/owl-carousel/owl-carousel/owl.carousel.js"></script>
+<script type="text/javascript" src="assets/js/plugins/owl-carousel.js"></script>
+<script type="text/javascript">
+    jQuery(document).ready(function() {
+        OwlCarousel.initOwlCarousel();
+    });
+</script>
+<!--[if lt IE 9]>
+    <script src="assets/plugins/respond.js"></script>
+    <script src="assets/plugins/html5shiv.js"></script>
+    <script src="assets/plugins/placeholder-IE-fixes.js"></script>
+<![endif]-->
+
+</body>
+
+<!-- Mirrored from htmlstream.com/preview/unify-v1.8/page_about.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 19 Nov 2015 07:57:00 GMT -->
+</html>
